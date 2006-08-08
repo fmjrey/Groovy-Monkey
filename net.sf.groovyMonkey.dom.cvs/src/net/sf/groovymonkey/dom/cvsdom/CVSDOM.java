5f5f623bde261990b5c2e068c0037b97a5c81c20
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2006-08-08 19:45:22 +0000

    *** empty log message ***
    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@132 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey.dom.cvs/src/net/sf/groovymonkey/dom/cvsdom/CVSDOM.java
+++ net.sf.groovyMonkey.dom.cvs/src/net/sf/groovymonkey/dom/cvsdom/CVSDOM.java
@@ -1,5 +1,16 @@
 package net.sf.groovymonkey.dom.cvsdom;
+import static org.eclipse.team.internal.ccvs.core.CVSProviderPlugin.getPlugin;
+import org.eclipse.team.internal.ccvs.core.ICVSRepositoryLocation;
 
 public class CVSDOM
 {
+    public ICVSRepositoryLocation getKnownRepository( final String locationString )
+    {
+        for( ICVSRepositoryLocation location : getPlugin().getKnownRepositories() )
+        {
+            if( location.getLocation( true ).equals( locationString ) )
+                return location;
+        }
+        return null;
+    }
 }
