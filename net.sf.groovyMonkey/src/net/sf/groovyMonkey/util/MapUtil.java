0438c375f11da320a04e8a49d61d1d8480bec9b6
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2006-11-30 16:45:55 +0000

    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@227 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey/src/net/sf/groovyMonkey/util/MapUtil.java
+++ net.sf.groovyMonkey/src/net/sf/groovyMonkey/util/MapUtil.java
@@ -1,6 +1,5 @@
 package net.sf.groovyMonkey.util;
 import static net.sf.groovyMonkey.util.ListUtils.list;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
