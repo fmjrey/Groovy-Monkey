f8cff2988f6c5d33220c4d7e1e5cfc36952a4a52
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2006-12-18 18:07:52 +0000

    EPL stuff.
    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@297 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey.tests/src/net/sf/groovymonkey/tests/ScriptMetadataTest.java
+++ net.sf.groovyMonkey.tests/src/net/sf/groovymonkey/tests/ScriptMetadataTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2006 Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bjorn Freeman-Benson - initial implementation
+ *     Ward Cunningham - initial implementation
+ *******************************************************************************/
 package net.sf.groovymonkey.tests;
 import static net.sf.groovyMonkey.GroovyMonkeyPlugin.FILE_EXTENSION;
 import static net.sf.groovyMonkey.GroovyMonkeyPlugin.MONKEY_DIR;
