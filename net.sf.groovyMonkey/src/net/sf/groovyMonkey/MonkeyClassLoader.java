6b2f2964cf4520b00334ee1735c60cbeaa2da9f1
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2006-12-18 17:48:35 +0000

    EPL stuff.
    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@293 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey/src/net/sf/groovyMonkey/MonkeyClassLoader.java
+++ net.sf.groovyMonkey/src/net/sf/groovyMonkey/MonkeyClassLoader.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bjorn Freeman-Benson - initial implementation
+ *     Ward Cunningham - initial implementation
+ *******************************************************************************/
+
 package net.sf.groovyMonkey;
 import static java.lang.reflect.AccessibleObject.setAccessible;
 import java.io.IOException;
