6336031f1d50b46f427443d454ac59e72128a1dc
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2007-11-16 23:23:54 +0000

    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@403 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey/src/net/sf/groovyMonkey/MonkeyClassLoader.java
+++ net.sf.groovyMonkey/src/net/sf/groovyMonkey/MonkeyClassLoader.java
@@ -16,31 +16,39 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
+import net.sf.groovyMonkey.util.ListUtil;
+import net.sf.groovyMonkey.util.MapUtil;
 
+@SuppressWarnings("unchecked")
 public class MonkeyClassLoader 
 extends ClassLoader 
 {
-	private final List< ClassLoader > loaders = new ArrayList< ClassLoader >();
-	private final Map< String, Class<?> > clases = new HashMap< String, Class<?> >();
+    private final List< ClassLoader > loaders = ListUtil.array();
+    private final Map< String, Class< ? > > clases = MapUtil.hashMap();
+    private final Map< String, URL > resources = MapUtil.hashMap();
     
-	public MonkeyClassLoader()
+    
+    public MonkeyClassLoader( final ClassLoader parentLoader )
+    {
+        super( parentLoader );
+    }
+    public MonkeyClassLoader()
     {
         super();
     }
-    public MonkeyClassLoader( final ClassLoader parent )
-    {
-        super( parent );
-    }
     public void add( final ClassLoader loader ) 
     {
-		loaders.add( loader );
-	}
-	@Override
+        loaders.add( loader );
+    }
+    public List< ClassLoader > getLoaders()
+    {
+        return ListUtil.array( loaders );
+    }
+    @Override
     public Class< ? > loadClass( final String name ) 
     throws ClassNotFoundException 
     {
@@ -48,11 +56,11 @@ extends ClassLoader
             return clases.get( name );
         for( final ClassLoader loader : loaders )
         {
-			try
+            try
             {
                 if( loader == null )
                     continue;
-                final Class<?> clase = loader.loadClass( name );
+                final Class< ? > clase = loader.loadClass( name );
                 if( clase != null )
                 {
                     clases.put( name, clase );
@@ -67,12 +75,12 @@ extends ClassLoader
             {
                 throw new RuntimeException( e );
             }
-		}
-        final Class<?> clase = super.loadClass( name );
+        }
+        final Class< ? > clase = super.loadClass( name );
         if( clase != null )
             return clase;
-		throw new ClassNotFoundException( name );
-	}
+        throw new ClassNotFoundException( name );
+    }
     @Override
     protected synchronized Class< ? > loadClass( final String name, 
                                                  final boolean resolve ) 
@@ -86,7 +94,7 @@ extends ClassLoader
             {
                 if( loader == null )
                     continue;
-                final Class<?> clase = invoke( loader, "loadClass", name, resolve );
+                final Class< ? > clase = ( Class< ? > )invoke( loader, "loadClass", name, resolve );
                 if( clase != null )
                 {
                     clases.put( name, clase );
@@ -98,7 +106,7 @@ extends ClassLoader
                 throw new RuntimeException( e );
             }
         }
-        final Class<?> clase = super.loadClass( name, resolve );
+        final Class< ? > clase = super.loadClass( name, resolve );
         if( clase != null )
             return clase;
         throw new ClassNotFoundException( name );
@@ -113,33 +121,37 @@ extends ClassLoader
         {
             if( loader == null )
                 continue;
-            Class< ? > clase = invoke( loader, "findClass", name );
+            Class< ? > clase = ( Class< ? > )invoke( loader, "findClass", name );
             if( clase != null )
             {
                 clases.put( name, clase );
                 return clase;
             }
-            clase = invoke( loader, "findLocalClass", name );
+            clase = ( Class< ? > )invoke( loader, "findLocalClass", name );
             if( clase != null )
             {
                 clases.put( name, clase );
                 return clase;
             }
         }
-        return super.findClass( name );
+        try
+        {
+            return super.findClass( name );
+        }
+        catch( final ClassNotFoundException cnfe )
+        {
+            throw new ClassNotFoundException( "Could not find class: " + name + ". " + cnfe.getMessage(), cnfe );
+        }
     }
-    private Class< ? > invoke( final ClassLoader loader, 
-                               final String methodName, 
-                               final String name )
+    private Object invoke( final ClassLoader loader, 
+                           final String methodName, 
+                           final String name )
     {
         try
         {
-            final Method method = loader.getClass().getDeclaredMethod( methodName, new Class<?>[] { String.class } );
+            final Method method = loader.getClass().getDeclaredMethod( methodName, new Class[] { String.class } );
             setAccessible( new Method[] { method }, true );
-            final Object object = method.invoke( loader, name );
-            if( object == null || !( object instanceof Class ) )
-                return null;
-            return ( Class<?> )object;
+            return method.invoke( loader, name );
         }
         catch( final SecurityException se )
         {
@@ -158,19 +170,16 @@ extends ClassLoader
         }
         return null;
     }
-    private Class< ? > invoke( final ClassLoader loader, 
-                               final String methodName, 
-                               final String name,
-                               final boolean resolve )
+    private Object invoke( final ClassLoader loader, 
+                           final String methodName, 
+                           final String name,
+                           final boolean resolve )
     {
         try
         {
-            final Method method = loader.getClass().getDeclaredMethod( methodName, new Class<?>[]{ String.class, boolean.class } );
+            final Method method = loader.getClass().getDeclaredMethod( methodName, new Class[]{ String.class, boolean.class } );
             setAccessible( new Method[]{ method }, true );
-            final Object object = method.invoke( loader, name, resolve );
-            if( object == null || !( object instanceof Class ) )
-                return null;
-            return ( Class<?> )object;
+            return method.invoke( loader, name, resolve );
         }
         catch( final SecurityException se )
         {
@@ -192,13 +201,55 @@ extends ClassLoader
     @Override
     protected URL findResource( final String name )
     {
+        if( resources.containsKey( name ) )
+            return resources.get( name );
+        for( final ClassLoader loader : loaders )
+        {
+            if( loader == null )
+                continue;
+            URL url = ( URL )invoke( loader, "findResource", name );
+            if( url != null )
+            {
+                resources.put( name, url );
+                return url;
+            }
+        }
         return super.findResource( name );
     }
+    
+    @Override
+    public URL getResource( final String resName )
+    {
+        final URL url = findResource( resName );
+        if( url != null )
+            return url;
+        return super.getResource( resName );
+    }
+    @SuppressWarnings("unchecked")
     @Override
     protected Enumeration< URL > findResources( final String name ) 
     throws IOException
     {
-        return super.findResources( name );
+        final Map< String, URL > resourceMap = MapUtil.linkedMap();
+        for( final ClassLoader loader : loaders )
+        {
+            if( loader == null )
+                continue;
+            final Enumeration< URL > enumeration = ( Enumeration< URL > )invoke( loader, "findResources", name );
+            while( enumeration.hasMoreElements() )
+            {
+                final URL element = ( URL )enumeration.nextElement();
+                if( !resourceMap.containsKey( element.getPath() ) )
+                    resourceMap.put( element.getPath(), element );
+            }
+        }
+        final Enumeration< URL > enumeration = super.findResources( name );
+        while( enumeration.hasMoreElements() )
+        {
+            final URL element = ( URL )enumeration.nextElement();
+            if( !resourceMap.containsKey( element.getPath() ) )
+                resourceMap.put( element.getPath(), element );
+        }
+        return new Vector< URL >( resourceMap.values() ).elements();
     }
-    
 }
