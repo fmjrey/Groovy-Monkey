675a12e0e1c3b526cb1c72a10c66d03c0833b5e8
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2008-03-18 05:12:05 +0000

    Added caching of resources and classes.
    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@441 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey/src/net/sf/groovyMonkey/BundleClassLoaderAdapter.java
+++ net.sf.groovyMonkey/src/net/sf/groovyMonkey/BundleClassLoaderAdapter.java
@@ -2,13 +2,18 @@ package net.sf.groovyMonkey;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.Map;
+import net.sf.groovyMonkey.util.MapUtil;
 import org.apache.commons.lang.ArrayUtils;
 import org.osgi.framework.Bundle;
 
+@SuppressWarnings("unchecked")
 public class BundleClassLoaderAdapter
 extends ClassLoader
 {
     private final Bundle[] bundles;
+    private final Map< String, URL > cachedResources = MapUtil.map();
+    private final Map< String, Class< ? > > cachedClasses = MapUtil.map();
     public BundleClassLoaderAdapter( final Bundle... bundles )
     {
         this.bundles = ( Bundle[] )ArrayUtils.clone( bundles );
@@ -33,15 +38,24 @@ extends ClassLoader
     private Class< ? > askBundles( final String name )
     throws ClassNotFoundException
     {
-        for( final Bundle bundle: bundles )
+        synchronized( cachedClasses )
         {
-            try
+            if( cachedClasses.containsKey( name ) )
+                return cachedClasses.get( name );
+            for( final Bundle bundle: bundles )
             {
-                return bundle.loadClass( name );
+                try
+                {
+                    final Class< ? > clase = bundle.loadClass( name );
+                    if( clase == null )
+                        continue;
+                    cachedClasses.put( name, clase );
+                    return clase;
+                }
+                catch( final ClassNotFoundException cnfe ) {}
             }
-            catch( final ClassNotFoundException cnfe ) {}
+            throw new ClassNotFoundException( "Class " + name + " not found in bundles: " + ArrayUtils.toString( bundles ) );
         }
-        throw new ClassNotFoundException( "Class " + name + " not found in bundles: " + ArrayUtils.toString( bundles ) );
     }
     @Override
     protected Class< ? > findClass( final String name )
@@ -52,15 +66,21 @@ extends ClassLoader
     @Override
     protected URL findResource( final String name )
     {
-        for( final Bundle bundle : bundles )
+        synchronized( cachedResources )
         {
-            final URL resource = bundle.getResource( name );
-            if( resource != null )
+            if( cachedResources.containsKey( name ) )
+                return cachedResources.get( name );
+            for( final Bundle bundle : bundles )
+            {
+                final URL resource = bundle.getResource( name );
+                if( resource == null )
+                    continue;
+                cachedResources.put( name, resource );
                 return resource;
+            }
+            return super.findResource( name );
         }
-        return super.findResource( name );
     }
-    @SuppressWarnings("unchecked")
     @Override
     protected Enumeration< URL > findResources( final String name )
     throws IOException
@@ -69,7 +89,7 @@ extends ClassLoader
         {
             try
             {
-                final Enumeration< URL > resource = ( Enumeration< URL > )bundle.getResources( name );
+                final Enumeration< URL > resource = bundle.getResources( name );
                 if( resource != null )
                     return resource;
             }
@@ -77,6 +97,7 @@ extends ClassLoader
         }
         return super.findResources( name );
     }
+    @Override
     public String toString()
     {
         return "bundles: " + ArrayUtils.toString( bundles );
