311e8e9f571979bdd6f6c82e7c36bbd98f974c4a
jervin <jervin@b4b37a63-7f15-0410-b854-ca377d9cf445>
2008-03-28 21:35:04 +0000

    git-svn-id: https://groovy-monkey.svn.sourceforge.net/svnroot/groovy-monkey/trunk@488 b4b37a63-7f15-0410-b854-ca377d9cf445

--- net.sf.groovyMonkey.site/UploadToSF.groovy
+++ net.sf.groovyMonkey.site/UploadToSF.groovy
@@ -1,5 +1,13 @@
+def clearCmd = "ssh jervin@shell.sourceforge.net /home/users/j/je/jervin/clearUpdate.sh"
+println "clearCmd: ${clearCmd}"
+def clearProcess = clearCmd.execute()
+println '  exitCode: ' + clearProcess.waitFor()
+println '  stdout: ' + clearProcess.getText()
+println '  stderr: ' + clearProcess.getErr().getText()
+
 new File( '.' ).eachFile
 { file ->
+    println "file: ${file} -> ${file.dump()}"
 	def path = file.getPath().tokenize( File.separator )
 	path.remove( 0 )
 	path = path.join( '/' )
@@ -16,3 +24,10 @@ new File( '.' ).eachFile
 	println '  stdout: ' + process.getText()
 	println '  stderr: ' + process.getErr().getText()
 }
+
+def chmodCmd = "ssh jervin@shell.sourceforge.net /home/users/j/je/jervin/chmodUpdate.sh"
+println "chmodCmd: ${chmodCmd}"
+def chmodProcess = chmodCmd.execute()
+println '  exitCode: ' + chmodProcess.waitFor()
+println '  stdout: ' + chmodProcess.getText()
+println '  stderr: ' + chmodProcess.getErr().getText()
